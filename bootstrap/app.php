677ea892fa9df45350031f7b109e2b4ee6619acf
Victor Wugajah Kichasu <vwkichasu@gmail.com>
2026-07-11 09:30:13 +0000

    Allow protected maintenance cleanup post

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'maintenance/clear-students',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
